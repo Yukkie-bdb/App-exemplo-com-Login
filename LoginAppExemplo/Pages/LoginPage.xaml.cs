e5740eb129bcfbdde91e9afad59f828dc5e832d1
eRIC bROLIO <ericbrolio@gmail.com>
2024-05-10 15:49:41 -0300

    Firts commit

---
+++ LoginAppExemplo/Pages/LoginPage.xaml.cs
@@ -0,0 +1,48 @@
+namespace LoginAppExemplo.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void BTNEntar_Clicked(object sender, EventArgs e)
+    {
+        string email = TXTEmail.Text;
+        string senha = TXTSenha.Text;
+
+        if (!string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(senha))
+        {
+            var usuario = await App.BancoDados.UserDataTable.obterUsuario(email, senha);
+
+            if (usuario != null)
+            {
+                await DisplayAlert("Sucesso", "Login efetuado com sucesso", "OK");
+                await Navigation.PushAsync(new HomePage());
+                App.Usuario = usuario;
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Usuário ou senha inválidos", "OK");
+                return;
+            }
+        }
+        else
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                await DisplayAlert("Erro", "Preencha o campo de e-mail", "OK");
+            }
+            else if (string.IsNullOrWhiteSpace(senha))
+            {
+                await DisplayAlert("Erro", "Preencha o campo de senha", "OK");
+            }
+        }
+    }
+
+    private async void BTNRegistar_Clicked(object sender, EventArgs e)
+    {
+       await Navigation.PushAsync(new EditUserPage());
+    }
+}
